986ec4784e267dd4c4ad282df44265fb43561b79
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-27 20:19:09 +0100

    Esercizio finito! Stile aggiunto

--- index.php
+++ index.php
@@ -9,12 +9,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="./styles/style.css">
     </head>
     <body>
-        <form action="./results.php" method="GET">
-            <input type="text-area" name="paragrafo" id="paragrafo" placeholder="Inserisci un paragrafo ...">
-            <input type="text" name="parola" id="parola" placeholder="Inserisci le parole da censurare ...">
-            <button type="submit">Invia</button>
-        </form>
+        <div class="container">
+            <form action="./results.php" method="GET">
+                <textarea class="input" rows="8" name="paragrafo" id="paragrafo" placeholder="Inserisci un paragrafo ..."></textarea>
+                <input class="input" type="text" name="parola" id="parola" placeholder="Inserisci le parole da censurare ...">
+                <button type="submit">Invia</button>
+            </form>
+        </div>
+        
     </body>
 </html>
--- results.php
+++ results.php
@@ -2,6 +2,7 @@
     $paragrafo = $_GET['paragrafo'];
     $parola = $_GET['parola'];
     $lunghezzaStringa = strlen($paragrafo);
+    $paragrafoFiltrato = str_replace($parola, '***', $paragrafo)
 ?>
 
 <!DOCTYPE html>
@@ -11,9 +12,22 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="./styles/style.css">
     </head>
     <body>
-        <p><?php echo $paragrafo ?></p>
-        <p>Il testo che hai scritto è composto da <strong><?php echo $lunghezzaStringa ?></strong> caratteri.</p>
+        <div class="container">
+            <div class="inner">
+                <h2>Risultato input</h2>
+                <div class="minor">
+                    <p><?php echo $paragrafo ?></p>
+                    <p>La parola da censurare che hai scelto è: <strong><?php echo $parola ?></strong></p>
+                    <p>Il testo che hai scritto è composto da <strong><?php echo $lunghezzaStringa ?></strong> caratteri.</p>
+                </div>
+                <h2>Testo filtrato</h2>
+                <div class="minor">
+                    <p><strong><?php echo $paragrafoFiltrato ?></strong></p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
